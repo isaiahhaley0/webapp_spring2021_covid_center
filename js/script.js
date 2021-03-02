e9e9ae6de48dc9b0ad9acecc4d9cc08b4d1cb593
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-01 17:53:14 -0700

    inital validation

--- js/script.js
+++ js/script.js
@@ -14,13 +14,32 @@ function updateCountryView(){
 
 
 function validateForm(){
-    var DoB = document.querySelector("#dob");
-    if(DoB.value == "")
-    {
-        /*show an error*/
+    var DoB = document.querySelector("#txtDOB");
+    var divDobError = document.querySelector("#divDoBError")
+
+ 
+   
+    if(DoB.value == ""){
+   
+        divDobError.classList.remove("invisible");
+        divDobError.innerHTML = "The DOB can't be empty!"
+        DoB.classList.add("hasError");
+   
     }
-
-
-
-    var DobDate = new Date(Dob.value);
-    var todayDate = new Date();
+    else {
+        var DoBDate = new Date(DoB.value);
+        var todayDate = new Date();
+        if(DoBDate >= todayDate) {
+            //Show error
+            divDoBError.classList.remove("invisible");
+            divDoBError.innerHTML = "DOB must be before today's date."
+            DoB.classList.add("hasError");
+             
+        }
+        else{
+            divDoBError.classList.add("invisible");
+            divDoBError.innerHTML = ""
+            DoB.classList.remove("hasError");
+        }
+    }
+}
